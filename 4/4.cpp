fc2cd37096fcea8d3de3a302ff12b3000c128189
akshaya0405 <akshayakarande0405@gmail.com>
2023-04-08 19:39:25 +0530

    added exp 4

---
+++ 4/4.cpp
@@ -0,0 +1,104 @@
+#include <iostream> 
+#include <climits> 
+#include <random> 
+#include <ctime> 
+using namespace std;
+
+void matrixChainOrder(int p[], int n, int m[][100], int s[][100]) 
+{ 
+    for(int i=1; i<=n; i++)
+    m[i][i] = 0; for(int l=2; l<=n; l++) 
+    {
+        for(int i=1; i<=n-l+1; i++) 
+        { int j = i+l-1;
+            m[i][j] = INT_MAX;
+
+            for(int k=i; k<=j-1; k++) 
+            {
+
+                int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j]; if(q < m[i][j]) 
+                {
+                    m[i][j] = q;
+
+                    s[i][j] = k;
+
+                }
+
+            }
+
+        }
+  
+    }
+
+}
+
+
+
+void printOptimalParenthesis(int s[][100], int i, int j) 
+{ 
+    if(i == j)
+    cout << "A" << i;
+    else 
+    {
+        cout << "("; printOptimalParenthesis(s, i, s[i][j]); printOptimalParenthesis(s, s[i][j]+1, j); cout << ")";
+    }
+
+}
+
+int main() 
+{ 
+    int p[8];
+    srand ( time(NULL) ); 
+    random_device rd; 
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(15, 46); 
+    for(int i=0; i<10; ++i)
+    p[i] = distr(gen);
+
+    int n = sizeof(p)/sizeof(p[0]) - 1; 
+    
+    int m[100][100];
+
+    int s[100][100];
+
+    matrixChainOrder(p, n, m, s);
+
+    cout << "\nOptimal Parenthesization: "; printOptimalParenthesis(s, 1, n);
+    cout << endl;
+
+    cout << "\nMinimum Number of Scalar Multiplications: " << m[1][n] << endl; 
+    cout << "\n\nm table:";
+    for(int a = 0; a < 8; a++)
+
+    {
+
+        for(int b = 0; b < 8; b++)
+
+        {
+
+            if(m[a][b] == 0){continue;}
+
+            cout << m[a][b] << " ";
+        
+        }
+
+        cout << endl;
+
+    }
+    cout << "\n\ns table:";
+
+    for(int a = 0; a < 10; a++)
+    {
+        for(int b = 0; b < 10; b++)
+        {
+            if(s[a][b] == 0){continue;}
+            cout << s[a][b] << " ";
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+
+}
